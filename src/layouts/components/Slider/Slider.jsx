45538d0d889f24c0a7fb8d3d0c737f5e52a3d2e3
Anh Phạm <pt.anh171020@gmail.com>
2023-02-24 22:21:57 +0700

    Layou UI Slider #2

--- src/layouts/components/Slider/Slider.jsx
+++ src/layouts/components/Slider/Slider.jsx
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faChevronRight,
     faChevronLeft,
 } from '@fortawesome/free-solid-svg-icons';
+import slider1 from '../../../assets/slider1.jpg';
+import slider2 from '../../../assets/slider2.jpg';
+import slider3 from '../../../assets/slider3.jpg';
+import slider4 from '../../../assets/slider4.jpg';
+import slider5 from '../../../assets/slider5.jpg';
 
 import style from './Slider.module.scss';
 
 const cx = classNames.bind(style);
 
+const SLIDERS = [
+    {
+        title: 'Slider 1',
+        path: slider1,
+    },
+    {
+        title: 'Slider 2',
+        path: slider2,
+    },
+    {
+        title: 'Slider 3',
+        path: slider3,
+    },
+    {
+        title: 'Slider 4',
+        path: slider4,
+    },
+    {
+        title: 'Slider 5',
+        path: slider5,
+    },
+];
+
 function Slider() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const sliderLength = SLIDERS.length;
+    const timeoutRef = useRef(null);
+
+    function resetTimeout() {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }
+
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+            () =>
+                setCurrentIndex((prevIndex) =>
+                    prevIndex === SLIDERS.length - 1 ? 0 : prevIndex + 1,
+                ),
+            4500,
+        );
+
+        return () => {
+            resetTimeout();
+        };
+    }, [currentIndex]);
+
     const handlePrevSlide = (e) => {
         e.preventDefault();
-        console.log('Slider Prev');
+        setCurrentIndex(
+            currentIndex === 0 ? sliderLength - 1 : currentIndex - 1,
+        );
     };
 
     const handleNextSlide = (e) => {
         e.preventDefault();
-        console.log('Slider Next');
+        setCurrentIndex(
+            currentIndex === sliderLength - 1 ? 0 : currentIndex + 1,
+        );
     };
 
     return (
@@ -28,7 +85,14 @@ function Slider() {
                     <FontAwesomeIcon icon={faChevronLeft} />
                 </button>
             </div>
-            <div className={cx('slider')}>Content Slide in here</div>
+
+            <div className={cx('slider')}>
+                <img
+                    src={SLIDERS[currentIndex].path}
+                    alt={SLIDERS[currentIndex].title}
+                />
+            </div>
+
             <div className={cx('next')}>
                 <button onClick={handleNextSlide}>
                     <FontAwesomeIcon icon={faChevronRight} />
